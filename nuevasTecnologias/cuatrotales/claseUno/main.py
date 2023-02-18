e4b320ced65b635ffdfceade5ca83e476a617453
cuatrotales <jfquinterogu14@gmail.com>
2023-02-18 13:03:31 -0500

    NT2 - Realizacion de suma en Python con casteo y peticion de valores por consola

--- nuevasTecnologias/cuatrotales/claseUno/main.py
+++ nuevasTecnologias/cuatrotales/claseUno/main.py
@@ -1,1 +1,7 @@
 # comentarios
+numeroUno = int(input('Digite el primer valor a sumar: '))
+numeroDos = int(input('Digite el segundo valor a sumar: '))
+
+totalSuma = numeroUno + numeroDos
+
+print(f'La suma de {numeroUno} + {numeroDos} = {totalSuma}')
