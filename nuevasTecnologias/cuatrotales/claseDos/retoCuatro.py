da58d43cbfb0d81cdce35b2036992a2377f8fc71
cuatrotales <jfquinterogu14@gmail.com>
2023-02-25 08:15:26 -0500

    NT - Cambio de entero a doble barra

--- nuevasTecnologias/cuatrotales/claseDos/retoCuatro.py
+++ nuevasTecnologias/cuatrotales/claseDos/retoCuatro.py
@@ -1,4 +1,4 @@
 juan = int(input("Digite la cantidad de dinero que tiene Juan: "))
-camila = int(juan/2)
-ricardo = int((juan + camila) / 2)
+camila = juan//2
+ricardo = (juan + camila) // 2
 print(f"Si puede. Juan tiene: {juan}, Camila tiene: {camila}, Ricardo tiene: {ricardo}. La suma total es: {int(juan + camila + ricardo)}")
